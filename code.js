481503fb2cc768e1fd6432372ef205daf4aa8ba3
Matt Shubat <mattshubat@gmail.com>
2021-02-23 21:36:22 -0500

    paddle bounce and cpu added

--- code.js
+++ code.js
@@ -3,8 +3,9 @@ var canvas;
 var canvasContext;
 
 const PADDLE_WIDTH = 10;
-const PADDLE_HEIGHT = 60;
-
+const PADDLE_HEIGHT = 100;
+var paddle1Y;
+var paddle2Y;
 
 const BALL_SIZE = 10;
 var ballX = 50;
@@ -12,11 +13,19 @@ var ballY = 100;
 var ballSpeedX = 1;
 var ballSpeedY = 1;
 
-const calculateMousePos = () => {
+const calculateMousePos = (evt) => {
   var rect = canvas.getBoundingClientRect();
   var root = document.documentElement;
+  let posObj = {
+    "clientX": evt.clientX,
+    "rect.left": rect.left,
+    "root.scrollLeft": root.scrollLeft
+  }
+  console.log(posObj);
+
   var mouseX = evt.clientX - rect.left - root.scrollLeft;
   var mouseY = evt.clientY - rect.top - root.scrollTop;
+
   return {
     x: mouseX,
     y: mouseY
@@ -28,32 +37,81 @@ window.onload = () => {
   canvas = document.getElementById("gameCanvas");
   canvasContext = canvas.getContext("2d");
 
+  paddle2Y = canvas.height/2-PADDLE_HEIGHT/2;
+  paddle1Y = canvas.height/2-PADDLE_HEIGHT/2;
   const fps = 144;
   setInterval(() => {
     updateEverything();
     drawEverything();
   }, 1000 / fps);
+
+  canvas.addEventListener('mousemove', (evt) => {
+    var mousePos = calculateMousePos(evt);
+    paddle1Y = mousePos.y;
+  })
 };
+
+const ballReset = () => {
+  ballSpeedX = ballSpeedX*-1;
+  ballX = canvas.width/2;
+  ballY = canvas.height/2;
+};
+
+const computerMovement = () => {
+  const paddleTop = paddle2Y-PADDLE_HEIGHT/2;
+  const paddleBottom = paddle2Y+PADDLE_HEIGHT/2;
+
+  if (paddleTop > ballY) {
+    paddle2Y -= 1;
+  } else if (paddleBottom < ballY) {
+    paddle2Y += 1;
+  }
+}
 
 const updateEverything = () => {
   // ball direction
   if (ballX <= BALL_SIZE/2|| ballX >= canvas.width-BALL_SIZE/2) {
-    ballSpeedX = ballSpeedX * -1;
+    ballReset();
   }
 
+  // hits left paddle
+  if (ballX === PADDLE_WIDTH && 
+      ballY >= paddle1Y-PADDLE_HEIGHT/2 &&
+      ballY <= paddle1Y+PADDLE_HEIGHT/2) {
+    ballSpeedX = -ballSpeedX
+  }
+
+  // hit right paddle
+  if (ballX === canvas.width-PADDLE_WIDTH &&
+      ballY >= paddle2Y-PADDLE_HEIGHT/2 &&
+      ballY <= paddle2Y+PADDLE_HEIGHT/2) {
+    ballSpeedX = -ballSpeedX;
+  }
+
+  // bounce off top and bottom
   if (ballY <= BALL_SIZE/2 || ballY >= canvas.height-BALL_SIZE/2) {
     ballSpeedY *= -1;
   }
 
+  computerMovement();
+
   // update
   ballX += ballSpeedX;
   ballY += ballSpeedY;
+
 };
 
 const drawEverything = () => {
+  // board
   colorRect(0, 0, canvas.width, canvas.height, "black");
-  colorRect(0, canvas.height/2-PADDLE_HEIGHT/2, PADDLE_WIDTH, PADDLE_HEIGHT, "white");
-  colorRect(canvas.width-10, canvas.height/2-PADDLE_HEIGHT/2, PADDLE_WIDTH, PADDLE_HEIGHT, "white");
+  
+  // left player
+  colorRect(0, paddle1Y-PADDLE_HEIGHT/2, PADDLE_WIDTH, PADDLE_HEIGHT, "white");
+  
+  // right player
+  colorRect(canvas.width-PADDLE_WIDTH, paddle2Y-PADDLE_HEIGHT/2, PADDLE_WIDTH, PADDLE_HEIGHT, "white");
+  
+  // ball
   colorCircle(ballX, ballY, BALL_SIZE, "white");
 };
 
